9ba3926ad3b1566d7244e7750802c9c41490dcc1
sid2290 <sudhanshunarang90@gmail.com>
2016-10-11 14:22:40 -0600

    solving minify problem #1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope, $log, $filter, $resource) {
+myApp.controller('mainController', ['$scope', '$log','$filter', '$resource', function($scope, $log, $filter, $resource) {
     
     console.log($resource);
     
-});
+}]);
