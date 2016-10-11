7f8170addb2023fa9adb8ea444b9c1ade1ff96df
sid2290 <sudhanshunarang90@gmail.com>
2016-10-10 23:06:18 -0600

    adding some angularjs built in services like log, filter and checking dependency injection...

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
+angularApp.controller('mainController', ['$scope','$log', '$filter', function ($scope,$log,$filter) {
     
+    console.log($scope);
+    console.log($log);
+    $log.log("Hello");
+    $log.info("This is some information");
+    $log.warn("Warning!!!");
+    
+    $scope.name = 'John';
+    $scope.formatname = $filter('uppercase')($scope.name);
+    console.log($scope.formatname);
 }]);
+
 
 
 
